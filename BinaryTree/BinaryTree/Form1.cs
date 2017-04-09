7fef1aa93f48d5fd464801b7372e3a9e0d69a3f0
Huynh Thanh Viet <huynhthviet@gmail.com>
2017-04-09 12:07:03 +0700

    Add files via upload

---
+++ BinaryTree/BinaryTree/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryTree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //declare myTree object
+        Tree myTree;
+        public string rst = null;
+
+        /*Create binary tree, display raw data and sorted data*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //add root value
+            Random rnd = new Random();
+            int num;
+            num = rnd.Next(0, 10000);
+            myTree = new Tree(num); 
+
+            //convert input
+            int n = Convert.ToInt32(txtbox1.Text);
+
+            //add to display at listBox1
+            List<int> _items = new List<int>(); 
+             _items.Add(num);
+
+            //add vertices into binary tree
+            for (int i = 1; i < n; i++)
+            {
+                num = rnd.Next(0, 10000);
+                myTree.AddRc(num);
+
+                //add to display
+                _items.Add(num);
+            }
+
+            //display raw data at listBox1
+            listBox1.DataSource = _items;
+            
+            //display sorted data at listBox2
+            string treestring = "";
+            myTree.Print(null, ref treestring);
+            listBox2.DataSource = myTree.sortedList;
+
+        }
+
+        /*Search and display result*/
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            rst = null;
+            myTree.track.Clear();
+            int n = Convert.ToInt32(txtBox2.Text);
+
+            //call Search
+            myTree.Search(null, n, ref rst);
+
+            if (!string.IsNullOrEmpty(rst))
+            {
+                lblSearch.Text = rst;
+            }
+            else
+            {
+                lblSearch.Text = "NOT FOUND " + n;
+            }
+
+            listBox3.DataSource = null;
+            listBox3.DataSource = myTree.track;
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Tree.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryTree
+{
+    class Tree
+    {
+        public Node top;
+        public List<int> sortedList = new List<int>();
+        public List<int> track = new List<int>();
+
+        public Tree()
+        {
+            top = null;
+        }
+
+        public Tree(int initial)
+        {
+            top = new Node(initial);
+        }
+
+
+        //Add node non-recursive add
+        public void Add(int value)
+        {
+            bool added = false;
+            
+            if (top == null)
+            {
+                //Add top
+                Node NewNode = new Node(value);
+                top = NewNode;
+                return;
+            }
+
+            Node currentnode = top;
+
+            //traverse the tree
+            while (added == false)
+            {
+                try
+                {
+                    //Go left
+                    if (value < currentnode.value)
+                    {
+                        if (currentnode.left == null)
+                        {
+                            Node NewNode = new Node(value);
+                            currentnode.left = NewNode;
+                            added = true;
+                        }
+                        else
+                        {
+                            currentnode = currentnode.left;
+                        }
+                    }
+
+                    //Go right
+                    if (value > currentnode.value)
+                    {
+                        if (currentnode.right == null)
+                        {
+                            Node NewNode = new Node(value);
+                            currentnode.right = NewNode;
+                            added = true;
+                        }
+                        else
+                        {
+                            currentnode = currentnode.right;
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Error: " + e);
+                }
+
+            }
+        }
+
+        //recursive add
+        public void AddRc(int value)
+        {
+            AddR(ref top, value);
+        }
+
+        //recursive add
+        private void AddR(ref Node N, int value)
+        {
+            //add root
+            if (N == null)
+            {
+                Node NewNode = new Node(value);
+                N = NewNode;
+                return;//end function
+            }
+
+            //Go left
+            if(value < N.value)
+            {
+                AddR(ref N.left, value);
+                return;//end function
+            }
+
+            //Go right
+            if(value > N.value)
+            {
+                AddR(ref N.right, value);
+                return;//end function
+            }
+        }
+
+        /*search and return string rst (result)*/
+        public void Search(Node N, int value, ref string rst)
+        {
+            //get top value
+            if(N == null)
+            {
+                N = top;
+            }
+
+            //found if end function
+            if(N.value == value)
+            {
+                track.Add(N.value);
+                rst = "Found " + N.value;
+                return;
+            }
+
+            //search left
+            else if(value < N.value && N.left != null)
+            {
+                track.Add(N.value);
+                Search(N.left, value, ref rst);
+            }
+
+            //search right
+            else if(value > N.value && N.right != null)
+            {
+                track.Add(N.value);
+                Search(N.right, value, ref rst);
+            }
+        }
+
+        public void Print(Node N, ref string s)
+        {
+            if (N == null)
+            {
+                N = top;
+            }
+
+            //add left to sortedList 
+            if(N.left != null)
+            {
+                Print(N.left, ref s);
+                s = s + N.value.ToString().PadLeft(3);
+                sortedList.Add(N.value);
+            }
+
+            //add node
+            else
+            {
+                s = s + N.value.ToString().PadLeft(3);
+                sortedList.Add(N.value);
+            }
+
+            //add right
+            if(N.right != null){
+                Print(N.right, ref s);
+            }
+        }
+    }
+}
